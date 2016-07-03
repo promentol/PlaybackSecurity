12bff083e32b582094a9b254d3086b2e40dc1625
promentol <promentol@gmail.com>
2016-07-03 20:58:33 +0400

    initial

---
+++ src/android/PlaybackSecurity.java
@@ -0,0 +1,21 @@
+import android.os.Bundle;
+import org.apache.cordova.*;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class CordovaApp extends CordovaActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.init();
+
+        WebView wv = (WebView) appView.getEngine().getView();
+        WebSettings ws = wv.getSettings();
+        ws.setMediaPlaybackRequiresUserGesture(false);
+        ws.setAllowFileAccess(true);
+
+        loadUrl(launchUrl);
+    }
+}
